41c7dcfe1f73d89fb5f9448b864c32f40040c019
Milind452 <milind.mohapatra@gmail.com>
2023-04-24 23:06:09 +0530

    close nav if window too small

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -1,8 +1,17 @@
 import { FaGithub } from "react-icons/fa";
 import Hamburger from "../../components/Hamburger/Hamburger";
 import "./Header.css";
+import useWindowSize from "../../hooks/useWindowSize";
 
 export default function Header({ isNavOpen, setIsNavOpen }) {
+    const { width } = useWindowSize();
+
+    if (width <= 700) {
+        setIsNavOpen(false);
+    } else {
+        setIsNavOpen(true);
+    }
+
     return (
         <section className="header">
             <div className="left">
@@ -15,7 +24,11 @@ export default function Header({ isNavOpen, setIsNavOpen }) {
                 <p>#css stash</p>
             </div>
             <div className="right">
-                <input placeholder="press &quot; ctrl + / &quot; to search" type="text" name="search" />
+                <input
+                    placeholder='press " ctrl + / " to search'
+                    type="text"
+                    name="search"
+                />
                 <a
                     rel="noreferrer"
                     href="https://github.com/milind452/css-stash"
