03a1a92062686c8b5579021957f9a99c0bb238a4
Milind452 <milind.mohapatra@gmail.com>
2023-04-29 17:25:27 +0530

    Add service to add new category

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -9,7 +9,6 @@ import { db } from "./firebase.config";
 async function getCategories() {
     try {
         const querySnapshot = await getDocs(collection(db, "category"));
-        console.log(querySnapshot);
         let categories = [];
         querySnapshot.forEach((doc) => {
             categories.push({ id: doc.id, category: doc.data().name });
@@ -20,4 +19,18 @@ async function getCategories() {
     }
 }
 
-export { getCategories };
+async function addCategory(categoryName) {
+    try {
+        const docRef = await addDoc(collection(db, "category"), {
+            name: categoryName,
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        console.log(
+            "An error occurred while trying to add new category: ",
+            error
+        );
+    }
+}
+
+export { getCategories, addCategory };
