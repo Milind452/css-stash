03c3bd54a4a85e7cbfce655987c7ce028aad5f5d
Milind452 <milind.mohapatra@gmail.com>
2023-04-29 17:55:40 +0530

    Remove test code from app

--- src/App.js
+++ src/App.js
@@ -3,20 +3,9 @@ import "./App.css";
 import Header from "./pages/Header/Header";
 import Nav from "./pages/Nav/Nav";
 import Overview from "./pages/Overview/Overview";
-import { getCategories } from "./services/categoryService";
 
 function App() {
     const [isNavOpen, setIsNavOpen] = useState(true);
-    const [categories, setCategories] = useState([]);
-    useEffect(() => {
-        console.log("App start...");
-        const fetchData = async () => {
-            const data = await getCategories();
-            setCategories(data);
-        };
-        fetchData();
-        console.log(categories);
-    }, []);
 
     return (
         <>
