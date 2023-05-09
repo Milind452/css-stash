b88db04d5293fd3f600e46d80b4e8ad656e01381
Milind452 <milind.mohapatra@gmail.com>
2023-05-09 22:22:57 +0530

    create initial card design

---
+++ src/components/Card/Card.js
@@ -0,0 +1,37 @@
+import "./Card.css";
+
+import {
+    SandpackLayout,
+    SandpackPreview,
+    SandpackProvider,
+} from "@codesandbox/sandpack-react";
+
+export default function Card({ name, html, css, js }) {
+    return (
+        <section className="card">
+            {/* <div>{html}</div>
+            <div>{css}</div>
+            <div>{js}</div> */}
+            <div className="preview">
+                <SandpackProvider
+                    template="static"
+                    files={{
+                        "/index.html": html,
+                        "/styles.css": css,
+                        "/index.js": js,
+                    }}
+                >
+                    <SandpackLayout
+                        style={{
+                            maxHeight: 200,
+                            maxWidth: 230,
+                        }}
+                    >
+                        <SandpackPreview />
+                    </SandpackLayout>
+                </SandpackProvider>
+            </div>
+            <div className="title">{name}</div>
+        </section>
+    );
+}
