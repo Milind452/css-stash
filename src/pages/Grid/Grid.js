197fc5b917874440da80c1d87f8fac0a785e71d2
Milind452 <milind.mohapatra@gmail.com>
2023-05-09 22:23:05 +0530

    create grid of cards

--- src/pages/Grid/Grid.js
+++ src/pages/Grid/Grid.js
@@ -1,7 +1,40 @@
 import { useParams } from "react-router";
 import "./Grid.css";
+import { useState, useEffect } from "react";
+import { getCodes } from "../../services/codeService";
+import Card from "../../components/Card/Card";
 
 export default function Grid() {
     const { category } = useParams();
-    return <div>Grid - {category}</div>;
+    const [codes, setCodes] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await getCodes(category);
+            setCodes(data);
+        };
+        fetchData();
+    }, [category]);
+
+    return (
+        <div id="codes" className="codes">
+            <h1>
+                {category.charAt(0).toUpperCase() +
+                    category.substring(1).toLowerCase()}
+            </h1>
+            <div className="grid">
+                {/* <div>Grid - {category}</div> */}
+                {/* <div>{JSON.stringify(codes)}</div> */}
+                {codes &&
+                    codes.map((code) => (
+                        <Card
+                            key={code.id}
+                            name={code.name}
+                            html={code.html}
+                            css={code.css}
+                            js={code.js}
+                        />
+                    ))}
+            </div>
+        </div>
+    );
 }
